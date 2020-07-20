51c5054c90b263bbc240da499d1f0a5581152f88
7 <zj175@139.com>
2020-07-20 16:49:09 +0800

    更新于2020年07月20日	16:49:09.31

---
+++ Config/config.py
@@ -0,0 +1,11 @@
+videoSuffixes = [
+    '.mp4', '.avi', '.vob', '.mkv', '.VOB',
+    '.flv', '.wmv', '.mov', '.rmvb', '.ts',
+    '.mpg', '.webm'
+]
+
+workDirs = [r'''
+
+D:\视频教程\Adobe\AU+AE+PR+C4D_2019
+
+'''[2:-2]]
---
+++ Storer/MySQL/Config/host - 副本.py
@@ -0,0 +1,6 @@
+host = '10.10.10.78'
+port = 30306
+database = 'syncFilesDatabase'
+user = 'rootAdmin'
+password = '1234567890'
+
